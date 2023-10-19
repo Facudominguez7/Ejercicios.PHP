8fa0454aad25e37540e6bd06b53c8e353d4b10db
unknown <facudominguez457@gmail.com>
2023-10-19 18:52:02 -0300

    ...

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+include('includes/conexion.php');
+conectar();
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -7,6 +12,7 @@
   <title>HOLA</title>
   <link rel="stylesheet" href="estilos.css" />
 </head>
+
 
 <body class="bg-dark">
   <nav class="navbar bg-body-tertiary">
@@ -18,17 +24,32 @@
       </form>
     </div>
   </nav>
-  <header></header>
+
   <main>
     <div class="d-flex justify-content-center mt-5">
       <div class="list-group w-50">
-        <a href="index.php?pagina=1" class="list-group-item list-group-item-action active" aria-current="true">
-          Ejercicio 1
+        <a href="index.php" class="list-group-item list-group-item-action active" aria-current="true">
+          Inicio
         </a>
-        <a href="index.php?pagina=2" class="list-group-item list-group-item-action">Ejercicio 2</a>
-        <a href="index.php?pagina=3" class="list-group-item list-group-item-action">Ejercicio 3</a>
-        <a href="index.php?pagina=4" class="list-group-item list-group-item-action">Ejercicio 4</a>
-        <a href="index.php?pagina=5" class="list-group-item list-group-item-action" aria-disabled="true">Ejercicio 5</a>
+        <?php
+        $sql = "SELECT * FROM menu";
+        $sql = mysqli_query($con, $sql);
+        if (mysqli_num_rows($sql) != 0) {
+          while ($r = mysqli_fetch_array($sql)) {
+        ?>
+            <a href="index.php?pagina=<?php echo $r['archivoItem'] ?>" class="list-group-item list-group-item-action active" aria-current="true">
+            <?php echo $r['nombreItem'] ?>
+            </a>
+        <?php
+          }
+        }
+        ?>
+        <!--
+          <a href="index.php?pagina=2" class="list-group-item list-group-item-action">Ejercicio 2</a>
+          <a href="index.php?pagina=3" class="list-group-item list-group-item-action">Ejercicio 3</a>
+          <a href="index.php?pagina=4" class="list-group-item list-group-item-action">Ejercicio 4</a>
+          <a href="index.php?pagina=5" class="list-group-item list-group-item-action" aria-disabled="true">Ejercicio 5</a>
+        -->   
         <div>
           <?php
           if (!empty($_GET['pagina'])) {
