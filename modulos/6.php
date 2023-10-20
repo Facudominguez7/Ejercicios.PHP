6db5bf4b1b9f84e3557efc02f1120df7b5a9e53f
Facundo Diaz Dominguez <facudominguez457@gmail.com>
2023-10-20 17:24:16 -0300

    Ejercicio 6 resuelto

--- modulos/6.php
+++ modulos/6.php
@@ -1,6 +1,6 @@
 <div class="form-floating mt-5 ms-5">
     <h3 style="color: white;" class="text-center mb-3">6 - Envío de datos de un FORMULARIO (controles text y submit)</h3>
-    <form method="post" action="paginaEjercicio6.php" style="color: white;" class="col-md-6 offset-md-3 text-center mb-3">
+    <form method="post" action="modulos/paginaEjercicio6.php" style="color: white;" class="col-md-6 offset-md-3 text-center mb-3">
         Ingrese el nombre:
         <br>
         <input type="text" name="nombre" class="mb-2">
